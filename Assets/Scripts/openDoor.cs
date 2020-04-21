9f31bf33f5a422b6e5c4d8bfa93ff6c270d64e82
gruby1265 <mikolaj.gosztyla@szkola.salezjanie.com>
2020-04-21 21:19:02 +0200

    nic

---
+++ Assets/Scripts/openDoor.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class openDoor : MonoBehaviour
+{
+    public Sprite openedDoorSpr;
+    SpriteRenderer sprRend;
+    bool hasChanged;
+    Collider2D coll;
+
+    void Start()
+    {
+        hasChanged = false;
+        sprRend = GetComponent<SpriteRenderer>();
+        coll = GetComponent<Collider2D>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (hasChanged) return;
+        if(other.tag == "Player")
+        {
+            if (other.GetComponent<playerMove>().doIHaveKey())
+            {
+                sprRend.sprite = openedDoorSpr;
+                coll.enabled = false;
+                hasChanged = true;
+            }
+        }
+    }
+}
